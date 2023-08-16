0e9ccca9869dda95f4e51c022467ded67ddf03d3
salsabileslam <salsabileslam16@gmail.com>
2023-08-17 02:43:56 +0300

    format date and delete item

--- lib/cubits/cubit/notes_cubit.dart
+++ lib/cubits/cubit/notes_cubit.dart
@@ -1,9 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 
-import '../../models/note_model.dart';
 import '../../constants/constant.dart' as constants;
+import '../../models/note_model.dart';
 
 part 'notes_state.dart';
 
